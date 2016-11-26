f73172d3b13950f1bc76318a7bbc6b9dfaa7172f
Cao Junquan <i@cdog.me>
2016-11-27 00:14:10 +0800

    Test CI

--- server/Main.go
+++ server/Main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
+	"github.com/CDog34/GBY/server/configs"
+	. "github.com/CDog34/GBY/server/routes"
+	. "github.com/CDog34/GBY/server/services"
 	"log"
 	"net/http"
-	. "github.com/CDog34/GBY/server/services"
-	. "github.com/CDog34/GBY/server/routes"
-	"github.com/CDog34/GBY/server/configs"
 )
 
 func main() {
+	log.Println("Server start!")
 	router := NewRouter(&RouteRules)
 	log.Printf("Server start on %s", config.Get("listenAddr").(string))
 	log.Fatal(http.ListenAndServe(config.Get("listenAddr").(string), router))
