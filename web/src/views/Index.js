f453b23b160fbb1c8e2795947818cc1d63e5709d
Cao Junquan <i@cdog.me>
2016-12-03 21:05:49 +0800

    Update Index.

--- web/src/views/Index.js
+++ web/src/views/Index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import styles from '../styles/Views/Index.scss';
 
 export class AppComponent extends React.Component {
+  componentWillMount() {
+    document.title = '首页-西道の狗窝';
+  }
+
   render() {
     return (
       <div className={styles.index}>
