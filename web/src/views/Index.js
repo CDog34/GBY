2d257c2dc8b08ab0255afb7e7b8013270fdd6f9a
Cao Junquan <i@cdog.me>
2016-12-03 21:46:15 +0800

    Update Index.

--- web/src/views/Index.js
+++ web/src/views/Index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import styles from '../styles/Views/Index.scss';
 
+class Action extends React.Component {
+  static propTypes = {
+    text: React.PropTypes.string,
+    href: React.PropTypes.string
+  };
+
+  render() {
+    return <a className={styles.action} href={this.props.href}>
+      {this.props.text}
+
+    </a>
+  }
+}
+
 export class AppComponent extends React.Component {
   componentWillMount() {
     document.title = '首页-西道の狗窝';
@@ -13,6 +27,14 @@ export class AppComponent extends React.Component {
           <h1>西道の狗窝</h1>
           <span>CDog's Kennel</span>
         </div>
+        <div className={styles.actions}>
+          <Action text="首页" href="http://cdog.me"/>
+          <Action text="文章"/>
+          <Action text="关于"/>
+        </div>
+        <p className={styles.footer}>
+          Power By <a href="https://github.com/CDog34/GBY" target="_blank">GBY</a>
+        </p>
       </div>
     );
   }
