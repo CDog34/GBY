1a6d14d1b14f3b487706789c905c60261b49df79
Cao Junquan <i@cdog.me>
2016-12-13 19:26:46 +0800

    Remove source Map

--- web/cfg/dist.js
+++ web/cfg/dist.js
@@ -16,7 +16,6 @@ let config = Object.assign({}, baseConfig, {
     path.join(__dirname, '../src/index')
   ],
   cache: false,
-  devtool: 'sourcemap',
   plugins: [
     new webpack.optimize.DedupePlugin(),
     new webpack.DefinePlugin({
