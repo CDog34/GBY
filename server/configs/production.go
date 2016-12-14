8721b1c514c0aef9426bf43ac382178bf2c9cfce
Cao Junquan <i@cdog.me>
2016-12-14 20:52:36 +0800

    remove Version.

--- server/configs/production.go
+++ server/configs/production.go
@@ -1,3 +1,16 @@
 package config
 
-var configProduction = ConfigRule{}
+import (
+	"fmt"
+	"os"
+)
+
+var configProduction = ConfigRule{
+	"listenAddr": fmt.Sprintf(":%s", string(os.Getenv("GBY_LISTEN_PORT"))),
+
+	"dbUrl":  os.Getenv("MONGODB_CONNECTION"),
+	"dbName": os.Getenv("MONGODB_INSTANCE_NAME"),
+
+	"sessionProvider":                "memory",
+	"accessControlAllowOriginHeader": "*",
+}
--- web/src/config/dist.js
+++ web/src/config/dist.js
@@ -3,7 +3,8 @@
 import baseConfig from './base';
 
 let config = {
-  appEnv: 'dist'  // feel free to remove the appEnv property here
+  appEnv: 'dist',  // feel free to remove the appEnv property here
+  apiBaseUrl: 'https://blog-api.cdog.me/'
 };
 
 export default Object.freeze(Object.assign({}, baseConfig, config));
